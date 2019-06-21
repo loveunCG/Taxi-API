dd90a6a068ddb12cbc89b30466d9eece1e902ea6
Phong <loveun1988@outlook.com>
2019-06-21 12:05:05 -0500

    update helper for html parser

--- app/Helper/ControllerHelper.php
+++ app/Helper/ControllerHelper.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 namespace App\Helpers;
 
@@ -8,7 +8,6 @@ use Illuminate\Support\Facades\Mail;
 
 class Helper
 {
-
     public static function upload_picture($picture)
     {
         $file_name = time();
@@ -18,37 +17,36 @@ class Helper
             $ext = $picture->getClientOriginalExtension();
             $picture->move(public_path() . "/uploads", $file_name . "." . $ext);
             $local_url = $file_name . "." . $ext;
-
             $s3_url = url('/').'/uploads/'.$local_url;
-            
             return $s3_url;
         }
         return "";
     }
 
 
-    public static function delete_picture($picture) {
-        File::delete( public_path() . "/uploads/" . basename($picture));
+    public static function delete_picture($picture)
+    {
+        File::delete(public_path() . "/uploads/" . basename($picture));
         return true;
     }
 
-    public static function generate_booking_id() {
+    public static function generate_booking_id()
+    {
         return Setting::get('booking_prefix').mt_rand(100000, 999999);
     }
 
-    public static function site_sendmail($user){
-
+    public static function site_sendmail($user)
+    {
         $site_details=Setting::all();
-       
 
-        
+
+
         Mail::send('emails.invoice', ['user' => $user,'site_details'=>$site_details], function ($mail) use ($user,$site_details) {
-           // $mail->from('harapriya@appoets.com', 'Your Application');
+            // $mail->from('harapriya@appoets.com', 'Your Application');
 
             $mail->to($user->user->email, $user->user->first_name.' '.$user->user->last_name)->subject('Invoice');
         });
 
         return true;
     }
-
 }
