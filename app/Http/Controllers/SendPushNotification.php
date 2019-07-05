c5650d7b6019e2d08d0157a04244b8f36809813f
Phong <loveun1988@outlook.com>
2019-07-05 09:24:30 -0500

    Update dispatcher issue

--- app/Http/Controllers/SendPushNotification.php
+++ app/Http/Controllers/SendPushNotification.php
@@ -2,212 +2,208 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use App\User;
 use App\ProviderDevice;
+use App\User;
 use Exception;
+use Illuminate\Http\Request;
 use Log;
 use Setting;
 
-class SendPushNotification extends Controller
-{
+class SendPushNotification extends Controller {
 	/**
-     * New Ride Accepted by a Driver.
-     *
-     * @return void
-     */
-    public function RideAccepted($request){
+	 * New Ride Accepted by a Driver.
+	 *
+	 * @return void
+	 */
+	public function RideAccepted($request) {
 
-    	return $this->sendPushToUser($request->user_id, trans('api.push.request_accepted'));
-    }
+		return $this->sendPushToUser($request->user_id, trans('api.push.request_accepted'));
+	}
 
-    /**
-     * Driver Arrived at your location.
-     *
-     * @return void
-     */
-    public function user_schedule($user){
+	/**
+	 * Driver Arrived at your location.
+	 *
+	 * @return void
+	 */
+	public function user_schedule($user) {
 
-        return $this->sendPushToUser($user, trans('api.push.schedule_start'));
-    }
+		return $this->sendPushToUser($user, trans('api.push.schedule_start'));
+	}
 
-    /**
-     * New Incoming request
-     *
-     * @return void
-     */
-    public function provider_schedule($provider){
+	/**
+	 * New Incoming request
+	 *
+	 * @return void
+	 */
+	public function provider_schedule($provider) {
 
-        return $this->sendPushToProvider($provider, trans('api.push.schedule_start'));
+		return $this->sendPushToProvider($provider, trans('api.push.schedule_start'));
 
-    }
+	}
 
-    /**
-     * New Ride Accepted by a Driver.
-     *
-     * @return void
-     */
-    public function UserCancellRide($request){
+	/**
+	 * New Ride Accepted by a Driver.
+	 *
+	 * @return void
+	 */
+	public function UserCancellRide($request) {
 
-        return $this->sendPushToProvider($request->provider_id, trans('api.push.user_cancelled'));
-    }
+		return $this->sendPushToProvider($request->provider_id, trans('api.push.user_cancelled'));
+	}
 
+	/**
+	 * New Ride Accepted by a Driver.
+	 *
+	 * @return void
+	 */
+	public function ProviderCancellRide($request) {
 
-    /**
-     * New Ride Accepted by a Driver.
-     *
-     * @return void
-     */
-    public function ProviderCancellRide($request){
+		return $this->sendPushToUser($request->user_id, trans('api.push.provider_cancelled'));
+	}
 
-        return $this->sendPushToUser($request->user_id, trans('api.push.provider_cancelled'));
-    }
+	/**
+	 * Driver Arrived at your location.
+	 *
+	 * @return void
+	 */
+	public function Arrived($request) {
 
-    /**
-     * Driver Arrived at your location.
-     *
-     * @return void
-     */
-    public function Arrived($request){
+		return $this->sendPushToUser($request->user_id, trans('api.push.arrived'));
+	}
 
-        return $this->sendPushToUser($request->user_id, trans('api.push.arrived'));
-    }
+	/**
+	 * Driver Arrived at your location.
+	 *
+	 * @return void
+	 */
+	public function Dropped($request) {
 
-     /**
-     * Driver Arrived at your location.
-     *
-     * @return void
-     */
-    public function Dropped($request){
+		Log::info(trans('api.push.dropped') . Setting::get('currency') . $request->payment->total . ' by ' . $request->payment_mode);
 
-        Log::info( trans('api.push.dropped').Setting::get('currency').$request->payment->total.' by '.$request->payment_mode);
+		return $this->sendPushToUser($request->user_id, trans('api.push.dropped') . Setting::get('currency') . $request->payment->total . ' by ' . $request->payment_mode);
+	}
 
-        return $this->sendPushToUser($request->user_id, trans('api.push.dropped').Setting::get('currency').$request->payment->total.' by '.$request->payment_mode);
-    }
+	/**
+	 * Money added to user wallet.
+	 *
+	 * @return void
+	 */
+	public function ProviderNotAvailable($user_id) {
 
-    /**
-     * Money added to user wallet.
-     *
-     * @return void
-     */
-    public function ProviderNotAvailable($user_id){
+		return $this->sendPushToUser($user_id, trans('api.push.provider_not_available'));
+	}
 
-        return $this->sendPushToUser($user_id,trans('api.push.provider_not_available'));
-    }
+	/**
+	 * New Incoming request
+	 *
+	 * @return void
+	 */
+	public function IncomingRequest($provider) {
 
-    /**
-     * New Incoming request
-     *
-     * @return void
-     */
-    public function IncomingRequest($provider){
+		return $this->sendPushToProvider($provider, trans('api.push.incoming_request'));
 
-        return $this->sendPushToProvider($provider, trans('api.push.incoming_request'));
+	}
 
-    }
-    
+	/**
+	 * Driver Documents verfied.
+	 *
+	 * @return void
+	 */
+	public function DocumentsVerfied($provider_id) {
 
-    /**
-     * Driver Documents verfied.
-     *
-     * @return void
-     */
-    public function DocumentsVerfied($provider_id){
+		return $this->sendPushToProvider($provider_id, trans('api.push.document_verfied'));
+	}
 
-        return $this->sendPushToProvider($provider_id, trans('api.push.document_verfied'));
-    }
+	/**
+	 * Money added to user wallet.
+	 *
+	 * @return void
+	 */
+	public function WalletMoney($user_id, $money) {
 
+		return $this->sendPushToUser($user_id, $money . ' ' . trans('api.push.added_money_to_wallet'));
+	}
 
-    /**
-     * Money added to user wallet.
-     *
-     * @return void
-     */
-    public function WalletMoney($user_id, $money){
+	/**
+	 * Money charged from user wallet.
+	 *
+	 * @return void
+	 */
+	public function ChargedWalletMoney($user_id, $money) {
 
-        return $this->sendPushToUser($user_id, $money.' '.trans('api.push.added_money_to_wallet'));
-    }
+		return $this->sendPushToUser($user_id, $money . ' ' . trans('api.push.charged_from_wallet'));
+	}
 
-    /**
-     * Money charged from user wallet.
-     *
-     * @return void
-     */
-    public function ChargedWalletMoney($user_id, $money){
+	/**
+	 * Sending Push to a user Device.
+	 *
+	 * @return void
+	 */
+	public function sendPushToUser($user_id, $push_message) {
 
-        return $this->sendPushToUser($user_id, $money.' '.trans('api.push.charged_from_wallet'));
-    }
+		try {
 
-    /**
-     * Sending Push to a user Device.
-     *
-     * @return void
-     */
-    public function sendPushToUser($user_id, $push_message){
+			$user = User::findOrFail($user_id);
 
-    	try{
+			if ($user->device_token != "") {
 
-	    	$user = User::findOrFail($user_id);
+				\Log::info('sending push for user : ' . $user->first_name);
 
-            if($user->device_token != ""){
+				if ($user->device_type == 'ios') {
 
-                \Log::info('sending push for user : '. $user->first_name);
+					return \PushNotification::app('IOSUser')
+						->to($user->device_token)
+						->send($push_message);
 
-    	    	if($user->device_type == 'ios'){
+				} elseif ($user->device_type == 'android') {
 
-    	    		return \PushNotification::app('IOSUser')
-    		            ->to($user->device_token)
-    		            ->send($push_message);
+					return \PushNotification::app('AndroidUser')
+						->to($user->device_token)
+						->send($push_message);
 
-    	    	}elseif($user->device_type == 'android'){
-    	    		
-    	    		return \PushNotification::app('AndroidUser')
-    		            ->to($user->device_token)
-    		            ->send($push_message);
+				}
+			}
 
-    	    	}
-            }
+		} catch (Exception $e) {
+			return $e;
+		}
 
-    	} catch(Exception $e){
-    		return $e;
-    	}
+	}
 
-    }
+	/**
+	 * Sending Push to a user Device.
+	 *
+	 * @return void
+	 */
+	public function sendPushToProvider($provider_id, $push_message) {
 
-    /**
-     * Sending Push to a user Device.
-     *
-     * @return void
-     */
-    public function sendPushToProvider($provider_id, $push_message){
+		try {
 
-    	try{
+			$provider = ProviderDevice::where('provider_id', $provider_id)->with('provider')->first();
 
-	    	$provider = ProviderDevice::where('provider_id',$provider_id)->with('provider')->first();
+			if ($provider->token != "") {
 
-            if($provider->token != ""){
-                
-                \Log::info('sending push for provider : '. $provider->provider->first_name);
+				\Log::info('sending push for provider : ' . $provider->provider->first_name);
 
-            	if($provider->type == 'ios'){
+				if ($provider->type == 'ios') {
 
-            		return \PushNotification::app('IOSProvider')
-        	            ->to($provider->token)
-        	            ->send($push_message);
+					return \PushNotification::app('IOSProvider')
+						->to($provider->token)
+						->send($push_message);
 
-            	}elseif($provider->type == 'android'){
-            		
-            		return \PushNotification::app('AndroidProvider')
-        	            ->to($provider->token)
-        	            ->send($push_message);
+				} elseif ($provider->type == 'android') {
 
-            	}
-            }
+					return \PushNotification::app('AndroidProvider')
+						->to($provider->token)
+						->send($push_message);
 
-    	} catch(Exception $e){
-    		return $e;
-    	}
+				}
+			}
 
-    }
+		} catch (Exception $e) {
+			return $e;
+		}
+
+	}
 
 }
